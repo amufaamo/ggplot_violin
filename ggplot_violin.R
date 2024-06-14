30c9b887f0c000be7ff477c0bdfb124bc5e073c1
Masakazu Ishikawa/石川昌和/いしかわまさかず <45814539+amufaamo@users.noreply.github.com>
2024-06-14 15:47:05 +0900

    Add files via upload

--- ggplot_violin.R
+++ ggplot_violin.R
@@ -1,5 +1,6 @@
 # 必要なパッケージを読み込む
 library(shiny)
+library(DT)
 library(ggplot2)
 
 # UI定義
@@ -8,26 +9,48 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
+      fileInput('file', 'Please upload csv file'),
       actionButton('iris_load', "Use iris dataset"),
-      selectInput("xvar", "X軸の変数を選択:", 
-                  choices = names(iris), 
-                  selected = names(iris)[1]),
-      selectInput("yvar", "Y軸の変数を選択:", 
-                  choices = names(iris)[1:4], 
-                  selected = names(iris)[2])
+      uiOutput("xvar_ui"),
+      uiOutput("yvar_ui")
     ),
     
     mainPanel(
+      DTOutput('table'),
       plotOutput("violinPlot")
     )
   )
 )
 
 # サーバー定義
-server <- function(input, output) {
+server <- function(input, output, session) {
+  csv <- eventReactive(input$file, {
+    req(input$file$datapath)
+    file_path <- input$file$datapath
+    read.csv(file_path)
+  })
+  
+  observeEvent(csv(), {
+    updateSelectInput(session, "xvar", choices = names(csv()), selected = names(csv())[1])
+    updateSelectInput(session, "yvar", choices = names(csv()), selected = names(csv())[2])
+  })
+  
+  output$xvar_ui <- renderUI({
+    selectInput("xvar", "X軸の変数を選択:", choices = NULL)
+  })
+  
+  output$yvar_ui <- renderUI({
+    selectInput("yvar", "Y軸の変数を選択:", choices = NULL)
+  })
+  
+  output$table <- renderDT({
+    datatable(csv())
+  })
   
   output$violinPlot <- renderPlot({
-    ggplot(iris, aes_string(x = input$xvar, y = input$yvar)) +
+    req(csv())
+    data <- csv()
+    ggplot(data, aes_string(x = input$xvar, y = input$yvar)) +
       geom_violin(trim = FALSE) +
       geom_jitter(width = 0.2, size = 1) +
       labs(x = input$xvar, y = input$yvar) +
